536de81629b4dc6cfc5e04bfab603034553612b5
SidDevelop <subha.krish1999@gmail.com>
2016-09-17 22:12:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/aboutme', function (req, res){
+   res.send("Details about me will be posted here") 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
