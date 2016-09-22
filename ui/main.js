16c494a9875ff9a767178e2a7e30da78d6c1b2b1
SidDevelop <subha.krish1999@gmail.com>
2016-09-22 20:34:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('The main.js file has loaded');
 var element=document.getElementById('text');
 element.innerHTML = 'Hey!';
+var img=document.getElementById('madi');
+img.onclick= function () {
+  img.style.marginRight = '100px'  
+};
