f2c6099af71bad12890fda024ccd488aec9f52f3
SidDevelop <subha.krish1999@gmail.com>
2016-09-22 20:48:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,11 @@ console.log('The main.js file has loaded');
 var element=document.getElementById('text');
 element.innerHTML = 'Hey!';
 var img=document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick= function () {
-  img.style.marginRight = '100px'  
+    var interval = setInterval(moveRight, 50);
 };
